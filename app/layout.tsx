5ee9242006bfcf3f410ad9f9c7563f24a32ff2a3
Volodymyr Melnyk <vovakpro13@gmail.com>
2023-07-24 10:02:29 +0300

    added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className="h-full">
+      <body className={inter.className + " h-full"}>
         <Header />
         {children}
+
+        <footer className="bg-black mt-6 h-[200px]">
+          <div className="container mx-auto px-6 flex items-center justify-center h-full">
+            <p className="text-white text-2xl">FoootFab 2023</p>
+          </div>
+        </footer>
       </body>
     </html>
   );
